7fddbdfbd1c853df2e8caeb2bb184e4570d32db2
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-08-14 11:10:00 +1200

    Git thinks I changed something...

---
+++ CrystalTickets/Assets/TestPlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Replace this; it was just for testing the platforms.
+// See https://www.youtube.com/watch?v=Xnyb2f6Qqzg
+public class TestPlayerController : MonoBehaviour {
+
+	//This will be our maximum speed as we will always be multiplying by 1
+	public float maxSpeed = 2f;
+	//to check ground and to have a jumpforce we can change in the editor
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		//set our grounded bool
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		float move = Input.GetAxis ("Horizontal");//Gives us of one if we are moving via the arrow keys
+		//move our Players rigidbody
+		rigidBody.velocity = new Vector3 (move * maxSpeed, rigidBody.velocity.y);	
+	}
+
+	void Update(){
+		//if we are on the ground and the space bar was pressed, change our ground state and add an upward force
+		if(grounded && Input.GetKeyDown (KeyCode.Space)){
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0,jumpForce));
+		}
+	}
+}
