8c9bed7fe478595eb00a93676d9ccd1017c59365
Sean <toyman93@gmail.com>
2015-08-24 15:21:08 +1200

    Empty player class

---
+++ CrystalTickets/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	int speed = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up key is pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down key is pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			print ("Left key is pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			print ("Right key is pressed");
+		}
+	}
+}
