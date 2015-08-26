1353933b60860acdeadb6113bc4228d15ba90485
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-08-27 07:51:44 +1200

    Basic player animations
    Still a bit messy and off. PlayerController script was from master I
    think - I needed some stuff for animations that wasn't in this branch.
    Note that shoot is bound to Q.

--- CrystalTickets/Assets/Scripts/TestPlayerController.cs
+++ CrystalTickets/Assets/Scripts/TestPlayerController.cs
@@ -5,51 +5,80 @@ using System.Collections;
 // See https://www.youtube.com/watch?v=Xnyb2f6Qqzg
 public class TestPlayerController : MonoBehaviour {
 
-//	//This will be our maximum speed as we will always be multiplying by 1
-//	public float maxSpeed = 2f;
-//	//to check ground and to have a jumpforce we can change in the editor
-//	bool grounded = false;
-//	public Transform groundCheck;
-//	float groundRadius = 0.2f;
-//	public LayerMask whatIsGround;
-//	public float jumpForce = 700f;
+    private Animator animator;
+    private bool isFacingRight;
 
-	float moveSpeed = 0.1f;
-	private Rigidbody2D rigidBody;
-	bool grounded;
-	float GroundDistance;
-	// Use this for initialization
-	void Start () {
-		// Create player on start
-		Player mainChar = new Player();
+    //This will be our maximum speed as we will always be multiplying by 1
+    public float maxSpeed;
+    //to check ground and to have a jumpforce we can change in the editor
+    bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
 
-		// Retrieve attached rigidbody object and store
-		rigidBody = GetComponent<Rigidbody2D> ();
+    // Should be set to Ground layer - put anything that you want to treat as ground on this layer
+    public LayerMask whatIsGround;
 
-	}
-	void OnCollisionStay(Collision collisionInfo){
-		grounded = true;
-		print ("hello");
-	}
-	// FixedUpdate is called every 0.2seconds regardless of frame processing time
-	void FixedUpdate () {
+    public float jumpForce = 700f;
+    private Rigidbody2D rigidBody;
 
-	}
+    // Use this for initialization
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        isFacingRight = true;
+    }
 
-	// Update is called once every frame
-	void Update(){
+    void FixedUpdate() {
+        //set our grounded bool
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			rigidBody.velocity = new Vector2 (2.0f, 0.0f);
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			rigidBody.velocity = new Vector2 (-2.0f, 0.0f);
-		}
-		if (grounded && Input.GetKey(KeyCode.Space)){
-			print ("Space pressed");
-			rigidBody.AddForce(Vector3.up * 100);
-		}
-		print (grounded);
-	}
+        float move = Input.GetAxis("Horizontal");//Gives us of one if we are moving via the arrow keys
+                                                 //move our Players rigidbody
+        rigidBody.velocity = new Vector3(move * maxSpeed, rigidBody.velocity.y);
+    }
 
+    void Update() {
+
+        //if we are on the ground and the space bar was pressed, change our ground state and add an upward force
+        if (grounded) {
+
+            bool isMovingDown = rigidBody.velocity.y < 0;
+
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+                // Updates the animations to show jumping
+                animator.SetBool("Jump", true);
+            } else if (isMovingDown) {
+                // Stop the 'jump' state if the player's about to hit the ground
+                animator.SetBool("Jump", false);
+            }
+        }
+
+        /* Putting the animation transitions here seems to make it more responsive than when it's in FixedUpdate(),
+           but this could use a bit more testing. */
+
+        float move = Input.GetAxis("Horizontal");
+
+        // Update the animations to show running if the player is moving
+        bool isRunning = move == 0 ? false : true;
+        animator.SetBool("Run", isRunning);
+
+        // Update animations to reflect which way the player is moving
+        bool changedDirection = move > 0 && !isFacingRight || move < 0 && isFacingRight;
+        if (changedDirection)
+            Flip();
+
+        // Shooting - doesn't work if you just set 'Shoot' to the value of Input.GetKeyDown(KeyCode.Q) (hence second condition)
+        if (Input.GetKeyDown(KeyCode.Q))
+            animator.SetBool("Shoot", true);
+        if (Input.GetKeyUp(KeyCode.Q))
+            animator.SetBool("Shoot", false);
+    }
+
+    void Flip() {
+        isFacingRight = !isFacingRight;
+        Vector3 flippedScale = transform.localScale;
+        flippedScale.x *= -1;
+        transform.localScale = flippedScale;
+    }
 }
