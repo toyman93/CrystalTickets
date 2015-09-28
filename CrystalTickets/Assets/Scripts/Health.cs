f481405d56418a19a26be26f8ef3fc82828cf329
Harriet Robinson-Chen <harrietrc@gmail.com>
2015-09-29 08:07:48 +1300

    Started refactoring / working on health system

---
+++ CrystalTickets/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public int startingHealth = 100;
+
+    protected int currentHealth;
+    protected bool isDead;
+    protected Animator animator;
+
+    void Awake() {
+        currentHealth = startingHealth;
+        isDead = false;
+    }
+
+    // Use this for initialization
+    protected virtual void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public virtual void Damage(int damage) {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !isDead)
+            Destroy(); // Kill the player/mob
+    }
+
+    public virtual void Heal(int health) {
+        currentHealth += health;
+    }
+
+    public virtual void Destroy() {
+        isDead = true;
+        animator.SetBool("Dead", true); // Play the death animation
+    }
+
+}
--- CrystalTickets/Assets/Scripts/PlayerHealth.cs
+++ CrystalTickets/Assets/Scripts/PlayerHealth.cs
@@ -3,54 +3,36 @@ using System.Collections;
 using System;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour, IDamageable {
-
-    public int startingHealth = 100;
-    private int currentHealth;
-    private bool isDead;
+public class PlayerHealth : Health {
 
     public Slider healthBar;
 
     // Required when the player dies (change state / disable)
     private PlayerController movementController;
-    private Animator animator;
 
-    void Awake() {
-        currentHealth = startingHealth;
-        isDead = false;
-    }
-
-	void Start () {
+	protected override void Start () {
+        base.Start();
         movementController = GetComponent<PlayerController>();
-        animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
 	
 	}
 
-    public void damage(int damage) {
-        currentHealth -= damage;
-        
-        // Update the UI
+    public override void Damage(int damage) {
+        base.Damage(damage);
         if (healthBar != null)
-            healthBar.value = Math.Max(currentHealth, 0); // Don't let the health bar drop below 0
-
-        // Kill the player
-        if (currentHealth <= 0 && !isDead)
-            destroy();
+            healthBar.value = Math.Max(currentHealth, 0); // Don't let the health bar drop below 
     }
 
-    public void heal(int health) {
-        currentHealth += health;
-
+    public override void Heal(int health) {
+        base.Heal(health);
         if (healthBar != null)
             healthBar.value = Math.Min(currentHealth, startingHealth); // Health shouldn't exceed the max
     }
 
-    public void destroy() {
-        isDead = true;
-        animator.SetBool("Dead", true); // Play the death animation
+    public override void Destroy() {
+        base.Destroy();
         movementController.enabled = false; // Turn off the controls
     }
 }
